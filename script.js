0078f5f617d957b181aeccc538443d4c9cb7e78a
usernameidk-ian <hehehaha121314uh@gmail.com>
2026-02-23 19:40:28 -0800

    Adjust send cooldown duration and input disabling
    Reduced send cooldown duration from 2000ms to 1000ms and removed chat input disabling during cooldown.

--- script.js
+++ script.js
@@ -650,12 +650,10 @@ let sendCooldown = false;
 function applySendCooldown() {
   sendCooldown = true;
   sendChat.disabled = true;
-  chatInput.disabled = true;
   setTimeout(() => {
     sendCooldown = false;
     sendChat.disabled = false;
-    chatInput.disabled = false;
-  }, 2000);
+  }, 1000);
 }
 const myGifs = ["gifs/1.gif","gifs/2.gif","gifs/3.gif","gifs/4.gif","gifs/5.gif","gifs/6.gif","gifs/7.gif","gifs/8.gif","gifs/9.gif","gifs/10.gif","gifs/11.gif","gifs/12.gif","gifs/13.gif","gifs/14.gif","gifs/15.gif","gifs/16.gif","gifs/17.gif","gifs/18.gif","gifs/19.gif","gifs/20.gif","gifs/21.gif","gifs/22.gif","gifs/23.gif","gifs/24.gif","gifs/25.gif"];
 
